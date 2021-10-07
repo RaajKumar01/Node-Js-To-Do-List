d9fa8dd3c583102c0459d89a08f6f138e801b839
Oblivion <69029430+RaajKumar01@users.noreply.github.com>
2021-10-08 00:48:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-mongoose.connect('mongo db connection url', { useNewUrlParser: true } );
+mongoose.connect('mongodb://localhost:27017/todolist', { useNewUrlParser: true } );
 
 const header = "Today";
 
